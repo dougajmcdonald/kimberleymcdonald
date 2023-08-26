0a99267046c40cfefd5dfea1aec34ebdc08cbfc3
Doug McDonald <dougajmcdonald@gmail.com>
2023-08-26 21:59:54 +0100

    update things

--- src/app/page.js
+++ src/app/page.js
@@ -3,10 +3,15 @@ import Image from "next/image"
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+      <div className="max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <h1>Hello I&apos;m Kimberley McDonald</h1>
         <p>This is totally my own website and noone else has had any input.</p>
       </div>
+      <section>
+        <p>
+          I&apos;m an indepedent artist who enjoys drawing and making things.
+        </p>
+      </section>
     </main>
   )
 }
