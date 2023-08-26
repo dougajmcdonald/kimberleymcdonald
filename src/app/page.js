bac13d5a9c37de6764a8fa4e95dd2f77ba519d83
Doug McDonald <dougajmcdonald@gmail.com>
2023-08-26 21:49:14 +0100

    apos

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <h1>Hello I'm Kimberley McDonald</h1>
+        <h1>Hello I&apos;m Kimberley McDonald</h1>
         <p>This is totally my own website and noone else has had any input.</p>
       </div>
     </main>
